5a03382fa8eefbee15171d54c1d7417411bf20ab
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2020-03-11 06:08:43 +1100

    Update Cart.vue

--- components/Cart.vue
+++ components/Cart.vue
@@ -1,7 +1,17 @@
 <template>
 	<div class="cart">
-		{{cart}}
+		<div v-if="cart.length > 1">
+			{{ cart.length > 1 ? 'items' : 'item' }}
+			<ul>
+				<li v-for="product in cart" :key="product.index">
+					{{product}}
+				</li>
+			</ul>
 		<a :href="contents">Checkout</a>
+		</div>
+		<div v-else>
+			Empty
+		</div>
 	</div>
 </template>
 
@@ -13,7 +23,12 @@ export default {
 		},
 		contents() {
 			return this.$store.state.checkoutURL
+		},
+		shopifyCart(){
+			this.$shopify.checkout.fetch(this.$store.state.checkoutID).then(checkout => {
+				return checkout
+			});
 		}
-	},
+	}
 }
 </script>
