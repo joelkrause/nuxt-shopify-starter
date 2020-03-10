1865c82fdc5274a2d425b7820ec6ee5c7a3cc5c7
Joel Krause <joel@raak.com.au>
2020-03-10 11:18:43 +1100

    updated product page variants

--- pages/product/_slug.vue
+++ pages/product/_slug.vue
@@ -1,12 +1,26 @@
 <template>
   <main>
-    <h1>{{product.title}}</h1>
-    <h2>${{product.variants[0].price}}</h2>
-    
-    <form>
-      <input type="number" name="qty" min="1" value="1" />
-      <button type="button" :data-id="product.id">Add To Cart</button>
-    </form>
+    <section class="product_header">
+      <div class="product_header-images">
+        <div class="image" v-for="image in product.images" :key="image.index">
+          <img :src="image.src" />
+        </div>
+      </div>
+      <div class="product_header-info">
+        <h1>{{product.title}}</h1>
+        <h2>${{product.variants[0].price}}</h2>
+
+        <form>
+          <input type="number" name="qty" min="1" value="1" />
+          <select v-if="product.variants.length > 1" id="product_id">
+            <option v-for="variant in product.variants" :key="variant.index" :value="variant.id">{{variant.title}}</option>
+          </select>
+          <input v-else type="hidden" :value="product.variants[0].id" />
+          <button type="button" :data-id="product.id">Add To Cart</button>
+        </form>
+      </div>
+    </section>
+    <div v-html="product.descriptionHtml"></div>
     <Footer />
   </main>
 </template>
@@ -34,3 +48,17 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.product_header{
+  display: flex;
+  &-images,&-info{
+    width: 50%;
+  }
+  &-images {
+    img{
+      width: 25vw;
+    }
+  }
+}
+</style>
